e64f287d6d49be0d3d0ad7fc2f9dcf330ec794bb
Manish J <manish.in.java@gmail.com>
2018-04-03 09:04:41 +0530

    Removed unnecessary enclosing braces.

--- src/main/java/org/springframework/data/multichain/annotation/MultiChainStream.java
+++ src/main/java/org/springframework/data/multichain/annotation/MultiChainStream.java
@@ -12,7 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 package org.springframework.data.multichain.annotation;
 
@@ -28,7 +27,7 @@ import java.lang.annotation.*;
 @Documented
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ ElementType.TYPE })
+@Target(ElementType.TYPE)
 public @interface MultiChainStream
 {
   /**
